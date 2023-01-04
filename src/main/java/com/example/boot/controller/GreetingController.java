1298022da6baec7109e06227440c3ddf9634b633
alexanderthunt <alexander056@revature.net>
2023-01-04 17:57:33 -0500

    added a testing 500 error code

--- src/main/java/com/example/boot/controller/GreetingController.java
+++ src/main/java/com/example/boot/controller/GreetingController.java
@@ -1,6 +1,8 @@
 package com.example.boot.controller;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,4 +23,9 @@ public class GreetingController {
         return "Hello " + name;
     }
     
+    @GetMapping("/bad")
+    public ResponseEntity<String> sendBadErrorCode() {
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
