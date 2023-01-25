b9ddb49409f650cfc9176590bb13dd869b7cca78
alexanderthunt <alexander056@revature.net>
2023-01-25 15:27:55 -0500

    change to memory leak thread make it infinite

--- src/main/java/com/example/boot/entities/LoginThread.java
+++ src/main/java/com/example/boot/entities/LoginThread.java
@@ -4,15 +4,21 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class LoginThread extends Thread {
+    Random rand = new Random();
+    ArrayList<ArrayList<Integer>> arrayOfArraysOfIntegers = new ArrayList<>();
+
     @Override
     public void run() {
-        ArrayList<Integer> list = new ArrayList<>();
-        Random rand = new Random();
-        try {
-            while (true) {
-                list.add(rand.nextInt(999));
+        while (true) {
+            ArrayList<Integer> list = new ArrayList<>();
+            try {
+                while (true) {
+                    list.add(rand.nextInt(999));
+                }
+            } catch (OutOfMemoryError e) {
+                arrayOfArraysOfIntegers.add(list);
+                System.out.println("new integer array added to the array of arrays");
             }
-        } catch (OutOfMemoryError e) {
         }
     }
 }
